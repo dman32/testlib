c0aed1acb696a39001878287745f7ab3ce2f056f
Dave Manley <dman636@gmail.com>
2013-02-26 16:21:20 -0500

    initial commit

---
+++ testlib/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using SEALib;
+
+namespace testlib
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            SEALib.TCP.Server.addSocket("heartbeat", 2055);
+            SEALib.TCP.Server.startListening("heartbeat", onAccept, 1);
+        }
+        public void onAccept(IAsyncResult ar)
+        {
+            SEALib.ErrorMessages.ThrowError("connected!", "test", ErrorMessages.Level.alert, null, null);
+        }
+    }
+}
