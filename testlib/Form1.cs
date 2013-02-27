95313e8182b32571a80a5ab8135ba99097646e92
Dave Manley <dman636@gmail.com>
2013-02-27 16:58:46 -0500

    Async callbacks

--- testlib/Form1.cs
+++ testlib/Form1.cs
@@ -12,15 +12,31 @@ namespace testlib
 {
     public partial class Form1 : Form
     {
+        public static int cnt = 0;
         public Form1()
         {
             InitializeComponent();
-            SEALib.TCP.Server.addSocket("heartbeat", 2055);
-            SEALib.TCP.Server.startListening("heartbeat", onAccept, 1);
+            SEALib.TCP.addSocket("heartbeat", 2055);
+            SEALib.TCP.addSocket("data", 2056);
+            SEALib.TCP.startListening("heartbeat", onAccept, onReceive, 1, 1024);
+            SEALib.TCP.startListening("data", onAccept, onReceive, 1, 16048);
         }
-        public void onAccept(IAsyncResult ar)
+        public void onAccept(string name)
         {
-            SEALib.ErrorMessages.ThrowError("connected!", "test", ErrorMessages.Level.alert, null, null);
+            SEALib.ErrorMessages.ThrowError(name + " connected!", "test", ErrorMessages.Level.alert, null, null);
+        }
+        public void onReceive(string name, byte[] bytes, int rec)
+        {
+            if (name=="data")
+                updateTextDelegate();
+        }
+        public void updateTextDelegate()
+        {
+            cnt++;
+            if (textBox1.InvokeRequired)
+                textBox1.Invoke(new MethodInvoker(delegate { textBox1.Text = cnt.ToString(); }));
+            else
+                textBox1.Text += cnt.ToString();
         }
     }
 }
